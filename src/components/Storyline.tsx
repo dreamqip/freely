20cbf5c8b3099c3d9f92101984aa052fbc959055
Vladyslav Chuchupak <v.chuchupak@gmail.com>
2023-01-08 20:15:54 +0100

    feat: storyline image shimmer effect

--- src/components/Storyline.tsx
+++ src/components/Storyline.tsx
@@ -1,10 +1,11 @@
 import type { FC } from 'react';
 import { parseSeriesDetails } from '@/utilities/parseSeriesDetails';
-import ImageWithFallback from '@/components/Image';
 import { imageBaseUrlW400 } from '@/services/themoviedb';
 import { parseMovieDetails } from '@/utilities/parseMovieDetails';
 import type { IMovie } from '@/types/movie';
 import type { ITvShow } from '@/types/series';
+import { shimmer, toBase64 } from '@/utilities/shimmer';
+import ImageLegacyWithFallback from '@/components/ImageLegacy';
 
 interface Props {
   series: IMovie | ITvShow;
@@ -18,14 +19,19 @@ const Storyline: FC<Props> = ({ series }) => {
 
   return (
     <div className='grid grid-cols-1 gap-8 pt-20 md:grid-cols-[250px_minmax(0,_1fr)]'>
-      <ImageWithFallback
-        src={`${imageBaseUrlW400}${series.poster_path}`}
-        alt={('title' in series && series.title) || series.name}
-        priority
-        className='relative mx-auto aspect-[2/3] rounded-xl md:mx-0'
-        width={250}
-        height={400}
-      />
+      <div className='relative mx-auto md:mx-0'>
+        <ImageLegacyWithFallback
+          src={`${imageBaseUrlW400}${series.poster_path}`}
+          alt={('title' in series && series.title) || series.name}
+          placeholder='blur'
+          blurDataURL={`data:image/svg+xml;base64,${toBase64(
+            shimmer(250, 400)
+          )}`}
+          className='aspect-[2/3] rounded-md object-cover'
+          width={250}
+          height={400}
+        />
+      </div>
       <div className='relative'>
         <h2 className='text-2xl dark:text-white md:text-3xl'>Storyline</h2>
         <h3 className='text-md mt-2 dark:text-primary-dark md:text-lg'>
