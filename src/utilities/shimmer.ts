f68baf17136a47c477aadc2e35f8cdef75869a9a
Vladyslav Chuchupak <v.chuchupak@gmail.com>
2023-01-08 20:05:29 +0100

    feat: update shimmer animation

--- src/utilities/shimmer.ts
+++ src/utilities/shimmer.ts
@@ -1,15 +1,21 @@
 export const shimmer = (w: number, h: number) => `
-<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-  <defs>
-    <linearGradient id="g">
-      <stop stop-color="#333" offset="20%" />
-      <stop stop-color="#222" offset="50%" />
-      <stop stop-color="#333" offset="70%" />
-    </linearGradient>
-  </defs>
-  <rect width="${w}" height="${h}" fill="#333" />
-  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
-  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+<svg width="${w}" height="${h}" xmlns="http://www.w3.org/2000/svg">
+    <defs>
+        <linearGradient x1="0%" y1="0%" x2="100%" y2="0%" id="a">
+            <stop stop-color="#333" offset="0%"/>
+            <stop stop-color="#222" offset="30%"/>
+            <stop stop-color="#111" offset="70%"/>
+            <stop stop-color="#222" offset="85%"/>
+            <stop stop-color="#333" offset="100%"/>
+        </linearGradient>
+    </defs>
+    <rect width="${w}" height="${h}" fill="#333" />
+    <rect x="0" y="0" width="${w}" height="${h}" fill="url(#a)">
+        <animate attributeName="x" repeatCount="indefinite" dur="1.25s" values="-${w};${w}" />
+        <animate attributeName="width" repeatCount="indefinite" dur="1.25s" values="${w};${
+  w * 2
+}" />
+    </rect>
 </svg>`;
 
 export const toBase64 = (str: string) =>
