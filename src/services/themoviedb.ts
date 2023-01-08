93151677bb7ea454e5fd72d066e0d4dfc2755fcb
Vladyslav Chuchupak <v.chuchupak@gmail.com>
2023-01-08 20:00:50 +0100

    feat: singleton API

--- src/services/themoviedb.ts
+++ src/services/themoviedb.ts
@@ -1,5 +1,4 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react';
-import { HYDRATE } from 'next-redux-wrapper';
+import useSWR from 'swr';
 import type { IMovie } from '@/types/movie';
 import type { ITvShow, ITvShows } from '@/types/series';
 import type { IPerson } from '@/types/person';
@@ -9,141 +8,211 @@ import type { IMovies } from '@/types/movies';
 export const imageBaseUrlOriginal = 'https://image.tmdb.org/t/p/original';
 export const imageBaseUrlW400 = 'https://image.tmdb.org/t/p/w400';
 
-export const movieApi = createApi({
-  reducerPath: 'movieApi',
-  baseQuery: fetchBaseQuery({
-    baseUrl: 'https://api.themoviedb.org/3/',
-  }),
-  extractRehydrationInfo: (action, { reducerPath }) => {
-    if (action.type === HYDRATE) {
-      return action.payload[reducerPath];
-    }
-  },
-  endpoints: (builder) => ({
-    getMovieById: builder.query<IMovie, number>({
-      query: (id) => ({
-        url: `movie/${id}`,
-        params: {
-          api_key: process.env.NEXT_PUBLIC_API_KEY,
-          append_to_response:
-            'keywords,videos,images,recommendations,similar,reviews,credits',
-          include_image_language: 'en,null',
-        },
-      }),
-    }),
-    searchMovies: builder.query<ISearch, string>({
-      query: (searchQuery) => ({
-        url: 'search/multi',
-        params: {
-          api_key: process.env.NEXT_PUBLIC_API_KEY,
-          query: searchQuery,
-          page: 1,
-        },
-      }),
-    }),
-    getActorById: builder.query<IPerson, number>({
-      query: (id) => ({
-        url: `person/${id}`,
-        params: {
-          api_key: process.env.NEXT_PUBLIC_API_KEY,
-          append_to_response: 'combined_credits,images',
-        },
-      }),
-    }),
-    getTvShowById: builder.query<ITvShow, number>({
-      query: (id) => ({
-        url: `tv/${id}`,
-        params: {
-          api_key: process.env.NEXT_PUBLIC_API_KEY,
-          append_to_response:
-            'keywords,videos,images,recommendations,similar,reviews,credits',
-          include_image_language: 'en,null',
-        },
-      }),
-    }),
-    getPopularTvShows: builder.query<ITvShows, number>({
-      query: (page = 1) => ({
-        url: 'tv/popular',
-        params: {
-          api_key: process.env.NEXT_PUBLIC_API_KEY,
-          page,
-        },
-      }),
-    }),
-    getPopularMovies: builder.query<IMovies, number>({
-      query: (page = 1) => ({
-        url: 'movie/popular',
-        params: {
-          api_key: process.env.NEXT_PUBLIC_API_KEY,
-          page,
-        },
-      }),
-    }),
-    getNowPlayingMovies: builder.query<IMovies, number>({
-      query: (page = 1) => ({
-        url: 'movie/now_playing',
-        params: {
-          api_key: process.env.NEXT_PUBLIC_API_KEY,
-          page,
-          language: 'en-US',
-        },
-      }),
-    }),
-    getTopRatedMovies: builder.query<IMovies, number>({
-      query: (page = 1) => ({
-        url: 'movie/top_rated',
-        params: {
-          api_key: process.env.NEXT_PUBLIC_API_KEY,
-          page,
-          language: 'en-US',
-        },
-      }),
-    }),
-    getLatestSeries: builder.query<ITvShows, void>({
-      query: () => ({
-        url: 'tv/latest',
-        params: {
-          api_key: process.env.NEXT_PUBLIC_API_KEY,
-          language: 'en-US',
-        },
-      }),
-    }),
-    getTopRatedSeries: builder.query<ITvShows, number>({
-      query: (page = 1) => ({
-        url: 'tv/top_rated',
-        params: {
-          api_key: process.env.NEXT_PUBLIC_API_KEY,
-          page,
-          language: 'en-US',
-        },
-      }),
-    }),
-  }),
-});
+class TMDBApi {
+  private static _instance: TMDBApi;
+  private readonly apiKey = process.env.API_KEY;
+  private readonly publicApiKey = process.env.NEXT_PUBLIC_API_KEY;
+  private readonly baseUrl = 'https://api.themoviedb.org/3';
 
-export const {
-  useGetMovieByIdQuery,
-  useLazySearchMoviesQuery,
-  useGetActorByIdQuery,
-  useGetTvShowByIdQuery,
-  useGetPopularTvShowsQuery,
-  useGetPopularMoviesQuery,
-  useGetNowPlayingMoviesQuery,
-  useGetTopRatedMoviesQuery,
-  useGetLatestSeriesQuery,
-  useGetTopRatedSeriesQuery,
-  util: { getRunningQueriesThunk },
-} = movieApi;
+  public get instance(): TMDBApi {
+    if (!TMDBApi._instance) {
+      TMDBApi._instance = new TMDBApi();
+    }
+    return TMDBApi._instance;
+  }
+
+  private fetcher = async (url: string, options: RequestInit) => {
+    const response = await fetch(url, options);
+
+    return await response.json();
+  };
+
+  public getMovieById = async (
+    id: number,
+    fetchOpt: RequestInit
+  ): Promise<IMovie | undefined> => {
+    const response = await fetch(
+      `${this.baseUrl}/movie/${id}?api_key=${this.apiKey}&append_to_response=keywords,videos,images,recommendations,similar,reviews,credits&include_image_language=en,null`,
+      fetchOpt
+    );
+
+    if (!response.ok) {
+      return undefined;
+    }
+
+    return await response.json();
+  };
+
+  public getSeriesById = async (
+    id: number,
+    fetchOpt: RequestInit
+  ): Promise<ITvShow | undefined> => {
+    const response = await fetch(
+      `${this.baseUrl}/tv/${id}?api_key=${this.apiKey}&append_to_response=keywords,videos,images,recommendations,similar,reviews,credits&include_image_language=en,null`,
+      fetchOpt
+    );
+
+    if (!response.ok) {
+      return undefined;
+    }
+
+    return await response.json();
+  };
+
+  public getPersonById = async (
+    id: number,
+    fetchOpt: RequestInit
+  ): Promise<IPerson | undefined> => {
+    const response = await fetch(
+      `${this.baseUrl}/person/${id}?api_key=${this.apiKey}&append_to_response=images,combined_credits`,
+      fetchOpt
+    );
+
+    if (!response.ok) {
+      return undefined;
+    }
+
+    return await response.json();
+  };
+
+  public multiSearchQuery = (query: string, page = 1) => {
+    const { data, isLoading, error } = useSWR<ISearch>(
+      query
+        ? `${this.baseUrl}/search/multi?api_key=${this.publicApiKey}&language=en-US&query=${query}&page=${page}&include_adult=true`
+        : null,
+      this.fetcher,
+      { keepPreviousData: true, revalidateOnFocus: false }
+    );
+
+    return {
+      data,
+      isLoading,
+      error,
+    };
+  };
+
+  public getTopRatedMovies = async (
+    page = 1,
+    fetchOpt: RequestInit
+  ): Promise<IMovies> => {
+    const response = await fetch(
+      `${this.baseUrl}/movie/top_rated?api_key=${this.apiKey}&page=${page}`,
+      fetchOpt
+    );
+    return await response.json();
+  };
+
+  public getPopularSeries = async (
+    page = 1,
+    fetchOpt: RequestInit
+  ): Promise<ITvShows> => {
+    const response = await fetch(
+      `${this.baseUrl}/tv/popular?api_key=${this.apiKey}&page=${page}`,
+      fetchOpt
+    );
+    return await response.json();
+  };
+
+  public getPopularSeriesClient = (page = 1) => {
+    const { data, isLoading, error } = useSWR<ITvShows>(
+      `${this.baseUrl}/tv/popular?api_key=${this.publicApiKey}&page=${page}`,
+      this.fetcher
+    );
+
+    return {
+      series: data,
+      isLoading,
+      error,
+    };
+  };
+
+  public getTopRatedSeries = async (
+    page = 1,
+    fetchOpt: RequestInit
+  ): Promise<ITvShows> => {
+    const response = await fetch(
+      `${this.baseUrl}/tv/top_rated?api_key=${this.apiKey}&page=${page}`,
+      fetchOpt
+    );
+
+    return await response.json();
+  };
+
+  public getPopularMovies = async (
+    page = 1,
+    fetchOpt: RequestInit
+  ): Promise<IMovies> => {
+    const response = await fetch(
+      `${this.baseUrl}/movie/popular?api_key=${this.apiKey}&page=${page}`,
+      fetchOpt
+    );
+
+    return await response.json();
+  };
+
+  public getPopularMoviesClient = (page = 1) => {
+    const { data, isLoading, error } = useSWR<IMovies>(
+      `${this.baseUrl}/movie/popular?api_key=${this.publicApiKey}&page=${page}`,
+      this.fetcher
+    );
+
+    return {
+      data,
+      isLoading,
+      error,
+    };
+  };
+
+  public getTrendingMovies = async (
+    page = 1,
+    fetchOpt: RequestInit
+  ): Promise<IMovies> => {
+    const response = await fetch(
+      `${this.baseUrl}/trending/movie/week?api_key=${this.apiKey}&page=${page}`,
+      fetchOpt
+    );
+
+    return await response.json();
+  };
+
+  public getTrendingSeries = async (
+    fetchOpt: RequestInit
+  ): Promise<ITvShows> => {
+    const response = await fetch(
+      `${this.baseUrl}/trending/tv/week?api_key=${this.apiKey}`,
+      fetchOpt
+    );
+
+    return await response.json();
+  };
+
+  public getNowPlayingMovies = async (
+    page = 1,
+    fetchOpt: RequestInit
+  ): Promise<IMovies> => {
+    const response = await fetch(
+      `${this.baseUrl}/movie/now_playing?api_key=${this.apiKey}&page=${page}`,
+      fetchOpt
+    );
+
+    return await response.json();
+  };
+}
+
+const TMDBInstance = new TMDBApi().instance;
 
 export const {
   getMovieById,
-  searchMovies,
-  getActorById,
-  getTvShowById,
-  getPopularTvShows,
-  getPopularMovies,
-  getNowPlayingMovies,
   getTopRatedMovies,
-  getLatestSeries,
+  getSeriesById,
+  getPopularSeries,
+  getPersonById,
   getTopRatedSeries,
-} = movieApi.endpoints;
+  getPopularMovies,
+  getTrendingMovies,
+  getTrendingSeries,
+  getNowPlayingMovies,
+  multiSearchQuery,
+  getPopularMoviesClient,
+  getPopularSeriesClient,
+} = TMDBInstance;
