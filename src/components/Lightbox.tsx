50f70907daa4069e0566e618820981798d2ea3df
Vladyslav Chuchupak <v.chuchupak@gmail.com>
2023-01-14 11:16:38 +0100

    fix: remove custom image

--- src/components/Lightbox.tsx
+++ src/components/Lightbox.tsx
@@ -1,13 +1,11 @@
 import type { FC } from 'react';
 import type { LightboxExternalProps } from 'yet-another-react-lightbox';
-import { shimmer, toBase64 } from '@/utilities/shimmer';
+import ReactLightbox from 'yet-another-react-lightbox';
 
 import 'yet-another-react-lightbox/styles.css';
 import 'yet-another-react-lightbox/plugins/thumbnails.css';
 
 import Thumbnails from 'yet-another-react-lightbox/plugins/thumbnails';
-import ReactLightbox from 'yet-another-react-lightbox';
-import ImageWithFallback from '@/components/Image';
 
 const Lightbox: FC<LightboxExternalProps> = ({
   open,
@@ -23,41 +21,41 @@ const Lightbox: FC<LightboxExternalProps> = ({
     slides={slides}
     animation={{ swipe: 200 }}
     carousel={{ finite: true }}
-    render={{
-      slide: (image, _, rect) => {
-        let width = rect.width;
-        let height = rect.height;
-
-        if (image.width && image.height) {
-          width = Math.round(
-            Math.min(rect.width, (rect.height / image.height) * image.width)
-          );
-          height = Math.round(
-            Math.min(rect.height, (rect.width / image.width) * image.height)
-          );
-        }
-
-        return (
-          <div style={{ position: 'relative', width, height }}>
-            <ImageWithFallback
-              fill
-              src={image.src}
-              loading='eager'
-              placeholder='blur'
-              blurDataURL={`data:image/svg+xml;base64,${toBase64(
-                shimmer(Number(width), Number(height))
-              )}`}
-              alt={image.alt || ''}
-              sizes={
-                typeof window !== 'undefined'
-                  ? `${Math.ceil((width / window.innerWidth) * 100)}vw`
-                  : `${width}px`
-              }
-            />
-          </div>
-        );
-      },
-    }}
+    // render={{
+    //   slide: (image, _, rect) => {
+    //     let width = rect.width;
+    //     let height = rect.height;
+    //
+    //     if (image.width && image.height) {
+    //       width = Math.round(
+    //         Math.min(rect.width, (rect.height / image.height) * image.width)
+    //       );
+    //       height = Math.round(
+    //         Math.min(rect.height, (rect.width / image.width) * image.height)
+    //       );
+    //     }
+    //
+    //     return (
+    //       <div style={{ position: 'relative', width, height }}>
+    //         <ImageWithFallback
+    //           fill
+    //           src={image.src}
+    //           loading='eager'
+    //           placeholder='blur'
+    //           blurDataURL={`data:image/svg+xml;base64,${toBase64(
+    //             shimmer(Number(width), Number(height))
+    //           )}`}
+    //           alt={image.alt || ''}
+    //           sizes={
+    //             typeof window !== 'undefined'
+    //               ? `${Math.ceil((width / window.innerWidth) * 100)}vw`
+    //               : `${width}px`
+    //           }
+    //         />
+    //       </div>
+    //     );
+    //   },
+    // }}
   />
 );
 
